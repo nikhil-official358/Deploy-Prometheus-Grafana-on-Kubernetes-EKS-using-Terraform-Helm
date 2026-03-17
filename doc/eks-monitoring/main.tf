332a69b69ab9aaf39f78402dd2dcaa0229f6bda3
nikhil-official358 <130959914+nikhil-official358@users.noreply.github.com>
2026-03-17 20:37:26 +0530

    Add files via upload

---
+++ doc/eks-monitoring/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "monitoring-cluster"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  create_cloudwatch_log_group = false
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = false
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.large"]
+      desired_size   = 2
+    }
+  }
+
+  access_entries = {
+    admin = {
+      principal_arn = "arn:aws:iam::420747713131:user/devops"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+}
+
+ 
